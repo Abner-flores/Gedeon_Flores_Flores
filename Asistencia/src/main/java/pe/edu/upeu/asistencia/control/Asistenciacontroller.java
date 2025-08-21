71048f349a1500b1908d0ad06dc93001c54e82b8
SebastianSumi <cspam7780@gmail.com>
2025-08-21 11:31:25 -0500

    lala

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/control/Asistenciacontroller.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/control/Asistenciacontroller.java
@@ -1,7 +1,23 @@
 package pe.edu.upeu.asistencia.control;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import org.springframework.stereotype.Controller;
+
+import javax.swing.*;
 
 @Controller
 public class Asistenciacontroller {
+    @FXML TextField txtnum1, txtnum2;
+    @FXML Label txtresult;
+
+    @FXML
+    private void sumar() {
+        double num1 = Double.parseDouble(txtnum1.getText());
+        double num2 = Double.parseDouble(txtnum2.getText());
+        double resultado = num1 + num2;
+
+        txtresult.setText(String.valueOf(resultado));
+    }
 }
