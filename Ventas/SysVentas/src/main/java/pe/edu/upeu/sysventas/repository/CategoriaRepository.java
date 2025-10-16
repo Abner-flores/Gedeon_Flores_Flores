06f756da92490a960b3252674cc75f45b38ca06c
SebastianSumi <cspam7780@gmail.com>
2025-10-16 10:44:26 -0500

    h2

---
+++ Ventas/SysVentas/src/main/java/pe/edu/upeu/sysventas/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package pe.edu.upeu.sysventas.repository;
+
+import pe.edu.upeu.sysventas.dto.ComboBoxOption;
+import pe.edu.upeu.sysventas.model.Categoria;
+
+import java.util.List;
+
+public interface CategoriaRepository extends ICrudGenericoRepository<Categoria, Long>{
+
+}
